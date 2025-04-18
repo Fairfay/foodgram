2b735835c6379a0f81deb2fcd5ee4533ead95a9e
Денис Тычин <dencat87@gmail.com>
2025-04-18 22:40:58 +0700

    fix

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework import status
 
 from djoser.views import UserViewSet
 
@@ -15,6 +18,26 @@ class CustomUserViewSet(UserViewSet):
         if self.action == 'create':
             return CustomUserCreateSerializer
         return CustomUserSerializer
+
+    @action(
+        detail=False,
+        methods=['put', 'patch'],
+        url_path='me/avatar',
+        permission_classes=[IsAuthenticated],
+        parser_classes=[JSONParser],
+    )
+    def avatar(self, request):
+        user = request.user
+        avatar_data = request.data.get('avatar')
+        if not avatar_data:
+            return Response(
+                {'error': 'Поле avatar обязательно.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        user.avatar = avatar_data
+        user.save()
+        serializer = CustomUserSerializer(user, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(detail=False, permission_classes=[IsAuthenticated])
     def subscriptions(self, request):
