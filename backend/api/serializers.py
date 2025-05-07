61b2a22d7827355185e91359f197e5e80041ac43
Денис Тычин <dencat87@gmail.com>
2025-05-07 13:39:48 +0700

    fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,5 @@
+import base64
+from django.core.files.base import ContentFile
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
@@ -9,6 +11,15 @@ from recipes.models import (
 )
 
 User = get_user_model()
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name=f'temp.{ext}')
+        return super().to_internal_value(data)
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
