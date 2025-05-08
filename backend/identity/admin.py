94e99aa93d4832ee238d7f15325ed7d754c57bff
Денис Тычин <dencat87@gmail.com>
2025-05-09 00:50:09 +0700

    fix

--- backend/identity/admin.py
+++ backend/identity/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Subscription
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_active', 'is_staff', 'date_joined')
+    ordering = ('username',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    search_help_text = 'Поиск по имени пользователя, email, имени или фамилии'
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    search_fields = ('user__username', 'user__email', 'author__username', 'author__email')
+    list_filter = ('user', 'author')
+    search_help_text = 'Поиск по имени пользователя или email подписчика/автора'
---
+++ backend/recipes/admin.py
@@ -0,0 +1,56 @@
+from django.contrib import admin
+from django.db.models import Count
+from .models import (
+    Favorite,
+    Ingredient,
+    Recipe,
+    RecipeIngredient,
+    ShoppingCart,
+    Tag,
+)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    min_num = 1
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'favorites_count')
+    search_fields = ('name', 'author__username', 'author__email')
+    list_filter = ('tags', 'author')
+    inlines = (RecipeIngredientInline,)
+
+    def favorites_count(self, obj):
+        return obj.favorites.count()
+    favorites_count.short_description = 'В избранном'
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
+    list_filter = ('user', 'recipe')
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
+    list_filter = ('user', 'recipe') 
